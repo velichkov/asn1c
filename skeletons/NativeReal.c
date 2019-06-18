1ae6a629ba6b8adf1b83e4cef11519050b03f41b
Vasil Velichkov <vvvelichkov@gmail.com>
2019-06-18 06:00:06 +0300

    aper: Fix NativeReal APER encode/decode
    Make it more or less the same as UPER encode/decode
    
    Fixes a tests-randomized failure with bundles/bundles/04-REAL-bundle.txt

--- skeletons/NativeReal.c
+++ skeletons/NativeReal.c
@@ -272,7 +272,7 @@ NativeReal_decode_aper(const asn_codec_ctx_t *opt_codec_ctx,
                        const asn_TYPE_descriptor_t *td,
                        const asn_per_constraints_t *constraints,
                        void **dbl_ptr, asn_per_data_t *pd) {
-	double *Dbl = (double *)*dbl_ptr;
+	double d;
 	asn_dec_rval_t rval;
 	REAL_t tmp;
 	void *ptmp = &tmp;
@@ -280,27 +280,20 @@ NativeReal_decode_aper(const asn_codec_ctx_t *opt_codec_ctx,
 
 	(void)constraints;
 
-	/*
-	 * If the structure is not there, allocate it.
-	 */
-	if(Dbl == NULL) {
-		*dbl_ptr = CALLOC(1, sizeof(*Dbl));
-		Dbl = (double *)*dbl_ptr;
-		if(Dbl == NULL)
-			ASN__DECODE_FAILED;
-	}
-
 	memset(&tmp, 0, sizeof(tmp));
-	rval = OCTET_STRING_decode_aper(opt_codec_ctx, td, NULL,
+	rval = OCTET_STRING_decode_aper(opt_codec_ctx, &asn_DEF_REAL, NULL,
 			&ptmp, pd);
 	if(rval.code != RC_OK) {
 		ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_REAL, &tmp);
 		return rval;
 	}
 
-	ret = asn_REAL2double(&tmp, Dbl);
+	ret = asn_REAL2double(&tmp, &d);
 	ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_REAL, &tmp);
 	if(ret) ASN__DECODE_FAILED;
+
+	if(NativeReal__set(td, dbl_ptr, d) < 0 )
+		ASN__DECODE_FAILED;
 
 	return rval;
 }
@@ -309,7 +302,7 @@ asn_enc_rval_t
 NativeReal_encode_aper(const asn_TYPE_descriptor_t *td,
                        const asn_per_constraints_t *constraints,
                        const void *sptr, asn_per_outp_t *po) {
-	double Dbl = *(const double *)sptr;
+	double Dbl = NativeReal__get_double(td, sptr);
 	asn_enc_rval_t erval = {0,0,0};
 	REAL_t tmp;
 
@@ -322,7 +315,8 @@ NativeReal_encode_aper(const asn_TYPE_descriptor_t *td,
 		ASN__ENCODE_FAILED;
 
 	/* Encode a DER REAL */
-	erval = OCTET_STRING_encode_aper(td, NULL, &tmp, po);
+	erval = OCTET_STRING_encode_aper(&asn_DEF_REAL, NULL, &tmp, po);
+
 	if(erval.encoded == -1)
 		erval.structure_ptr = sptr;
 
